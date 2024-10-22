0f971c8fdbc53b5e51308ccae2fbedf8e5954a40
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-10-22 16:26:35 +0000

    fixed functions

--- ChoosYourOwnAdventureThingy/ChooseYourOwnAdventureThingy.java
+++ ChoosYourOwnAdventureThingy/ChooseYourOwnAdventureThingy.java
@@ -10,7 +10,7 @@ public class ChooseYourOwnAdventureThingy
         }
     }
 
-    public static boolean promptYesOrNo()
+    public static boolean promptYesOrNo() //prompts yes or no and returns true or false depending
     {
         Scanner sc = new Scanner(System.in);
         boolean validAnswer = false;
@@ -20,11 +20,35 @@ public class ChooseYourOwnAdventureThingy
             System.out.println("=====================================================");
             System.out.print("Answer yes or no: ");
             userAnswer = sc.nextLine();
-            if ((userAnswer == "yes") || (userAnswer == "no")) {
+            if ((userAnswer.equals("yes")) || (userAnswer.equals("no"))) {//checking if dude typed "yes or "no"
                 validAnswer = true;
+            } else {
+                System.out.println("Plz type exactly either \"yes\" or \"no\". ");
             }
         }
-        if (userAnswer == "yes") {
+        if (userAnswer.equals("yes")) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean prompt(String optionA, String optionB) //prompts a or b and returns true if 1st answer, false if 2nd answer
+    {
+        Scanner sc = new Scanner(System.in);
+        boolean validAnswer = false;
+        String userAnswer = "";
+        while (!validAnswer)
+        {
+            System.out.println("=====================================================");
+            System.out.print("Answer "+optionA+" or "+optionB+": ");
+            userAnswer = sc.nextLine();
+            if ((userAnswer.equals("yes")) || (userAnswer.equals("no"))) {//checking if dude typed correctly
+                validAnswer = true;
+            } else {
+                System.out.println("Plz type exactly either \""+optionA+"\" or \""+optionB+"\". ");
+            }
+        }
+        if (userAnswer.equals(optionA)) {
             return true;
         }
         return false;
