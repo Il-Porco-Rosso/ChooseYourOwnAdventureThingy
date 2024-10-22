ca8d553a6a9c2162606c6e458f9611ac513360d6
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-10-22 17:00:26 +0000

    finished fuctions

--- ChoosYourOwnAdventureThingy/ChooseYourOwnAdventureThingy.java
+++ ChoosYourOwnAdventureThingy/ChooseYourOwnAdventureThingy.java
@@ -4,11 +4,20 @@ public class ChooseYourOwnAdventureThingy
 {
     public static void main(String[] args)
     {
-        System.out.println("deez");
+        System.out.println("Would you like to go fishing?");
         if (promptYesOrNo()) {
-            System.out.println("nuts");
+            System.out.println("You cast your rod, \nbut you forget your bait. \nWould you like to reel in?");
+            if (promptYesOrNo()) {
+                
+            }
         }
+        border(0);
+        System.out.println("The end");
     }
+
+    //==========================================================================================================//
+    //----------------------------------------------------------------------------------------------------------//
+    //==========================================================================================================//
 
     public static boolean promptYesOrNo() //prompts yes or no and returns true or false depending
     {
@@ -42,7 +51,7 @@ public class ChooseYourOwnAdventureThingy
             System.out.println("=====================================================");
             System.out.print("Answer "+optionA+" or "+optionB+": ");
             userAnswer = sc.nextLine();
-            if ((userAnswer.equals("yes")) || (userAnswer.equals("no"))) {//checking if dude typed correctly
+            if ((userAnswer.equals(optionA)) || (userAnswer.equals(optionB))) {//checking if dude typed correctly
                 validAnswer = true;
             } else {
                 System.out.println("Plz type exactly either \""+optionA+"\" or \""+optionB+"\". ");
@@ -52,5 +61,39 @@ public class ChooseYourOwnAdventureThingy
             return true;
         }
         return false;
+    }
+
+    public static int prompt(String optionA, String optionB, String optionC) //prompts a, b, or c and returns 1, 2, or 3 respectively
+    {
+        Scanner sc = new Scanner(System.in);
+        boolean validAnswer = false;
+        String userAnswer = "";
+        while (!validAnswer)
+        {
+            System.out.println("=====================================================");
+            System.out.print("Answer "+optionA+", "+optionB+", or "+optionC+": ");
+            userAnswer = sc.nextLine();
+            if ((userAnswer.equals(optionA)) || (userAnswer.equals(optionB)) || (userAnswer.equals(optionC))) {//checking if dude typed correctly
+                validAnswer = true;
+            } else {
+                System.out.println("Plz type exactly either\""+optionA+"\", \""+optionB+"\", or \""+optionC+"\". ");
+            }
+        }
+        if (userAnswer.equals(optionA)) {
+            return 1;
+        } else if (userAnswer.equals(optionB)) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+    public static void border(int type) {
+        String borders[] = {"====================================================================================\n------------------------------------------------------------------------------\n====================================================================================", 
+                            "==============================================", 
+                            "---------------------------------------", 
+                            "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=", 
+                            ">----------------------<", "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n"};
+        System.out.println(borders[type]);
     }
 }
